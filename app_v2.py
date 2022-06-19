e385c86b0cc98a3474983a2a788d1740d3bc370e
“Rafaelx” <meow7747@gmail.com>
2022-06-19 14:08:31 +0700

    App 3 Pages

--- app_v2.py
+++ app_v2.py
@@ -24,65 +24,85 @@ if 'games' not in st.session_state:
   
 if 'time' not in st.session_state:
   st.session_state['time'] = []
+
+if 'next' not in st.session_state:
+  st.session_state['next'] = False
   
 if 'predict' not in st.session_state:
   st.session_state['predict'] = False
 
-def collect_data():
+def collect_game():
   res = st.multiselect("Choose your favorite game ", (st.session_state['games']))
 
-  time = st.slider("How much time you use to play in scores", max_value = 10.0, value = 5.0, step = 0.5)
-
-  Next = st.button("Next")
-  if Next:
+  Select = st.button("Select")
+  if Select:
     st.session_state['ans'] = res
-    st.session_state['time'].append(random.uniform(time-0.5, time+0.5))
     st.session_state['suggest'] = similiar[similiar['Ori_game'] == st.session_state['ans'][-1]]['Name']
 
   st.write("Similiar")
   st.write(st.session_state['suggest'])
-  Submit = st.button("Submit")
-  if Submit:
-    st.session_state['predict'] = True
-    st.button("Go !!!")
+  if len(st.session_state['ans']) >0:
+    Next = st.button("Next")
+    if Next: 
+      st.session_state['next'] = Next
+    
+def collect_time():
+  with st.form("my_form"):
+    time = [None] * len(st.session_state['ans'])
+    for i in range(len(st.session_state['ans'])):
+      st.write(st.session_state['ans'][i])
+      time[i] = st.slider("How much time you use to play in scores", max_value = 10.0, value = 5.0, step = 0.5, key = i)
+      time[i] = random.uniform(time[i] -0.5, time[i] +0.5)
+      
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+      st.session_state['time'] = time
+      df = pd.read_csv("./Name_id.csv")
+      for i in range(len(st.session_state['ans'])):
+        st.session_state['ans'][i] = df[df['item_name'] == st.session_state['ans'][i]]['item_id'].unique()[0]
+      st.session_state['predict'] = True
 
-# def predict():
-#   df = pd.read_csv("./Scaled_data_without_0.csv")
-#   table = pd.read_csv('./Encoded_df_with_newplay.csv', index_col = 0)
-#   ansAll = pd.read_csv('./ans0_51135.csv', index_col=0)
-
-#   for i in range(len(st.session_state['ans'])):
-#     st.session_state['ans'][i] = df[df['item_name'] == st.session_state['ans'][i]]['item_id'].unique()[0]
+def predict():
+  df_1 = pd.read_csv("./Df_1.csv")
+  df_2 = pd.read_csv("./Df_2.csv")
+  df = pd.concat([df_1, df_2])
+  ansAll = pd.read_csv('./ans0_51135.csv', index_col=0)
         
-#   new = pd.DataFrame()
-#   new['user_id'] = 'New player GG123'
-#   new['item_id'] = st.session_state['ans']
-#   new['playtime_scaled'] = st.session_state['time']
-#   new['user_id'] = 'New player" GG123'
+  # new = pd.DataFrame()
+  # new['user_id'] = 'New player GG123'
+  # new['item_id'] = st.session_state['ans']
+  # new['playtime_scaled'] = st.session_state['time']
+  # new['user_id'] = 'New player" GG123'
+  
+  st.write(st.session_state['ans'])
+  st.write(st.session_state['time'])
   
 
-#   keep = pd.DataFrame(columns=['A', 'B'])
-#   ch = pd.DataFrame()
+  # keep = pd.DataFrame(columns=['A', 'B'])
+  # ch = pd.DataFrame()
 
-#   for i in range(new.shape[0]):
-#       table.loc['New player GG123',new.iloc[i]['item_id']] = new.iloc[i]['playtime_scaled']
-#       ch = pd.concat([ch, df[df['item_id'] == new.iloc[i]['item_id']]])
-#   table = table.fillna(0)
+  # for i in range(new.shape[0]):
+  #     table.loc['New player GG123',new.iloc[i]['item_id']] = new.iloc[i]['playtime_scaled']
+  #     ch = pd.concat([ch, df[df['item_id'] == new.iloc[i]['item_id']]])
+  # table = table.fillna(0)
 
-#   for i in ch['user_id'].unique():
-#     if (table[table.index == i].empty == False):
-#       keep.loc[keep.shape[0]] = [i,distance.euclidean(table[table.index == 'New player GG123'], table[table.index == i])]
+  # for i in ch['user_id'].unique():
+  #   if (table[table.index == i].empty == False):
+  #     keep.loc[keep.shape[0]] = [i,distance.euclidean(table[table.index == 'New player GG123'], table[table.index == i])]
 
-#   similiar_user = keep.sort_values(by ='B', ascending = False).iloc[0]['A']
+  # similiar_user = keep.sort_values(by ='B', ascending = False).iloc[0]['A']
     
-#   ans_name = []
+  # ans_name = []
   
-#   for i in ansAll[ansAll['user_id'] == similiar_user]['item_id'].unique():
-#     ans_name.append(df[df['item_id'] == i]['item_name'].unique()[0])
+  # for i in ansAll[ansAll['user_id'] == similiar_user]['item_id'].unique():
+  #   ans_name.append(df[df['item_id'] == i]['item_name'].unique()[0])
     
-#   st.write(ans_name)
+  # st.write(ans_name)
     
 if st.session_state['predict'] == False:
-  collect_data()
+  if st.session_state['next'] == False:
+    collect_game()
+  else:
+    collect_time()
 else:
   predict()
